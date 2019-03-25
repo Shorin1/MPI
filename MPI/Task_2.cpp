579cd4487cf11aaa3df490e23564f2e6c43bd519
Дмитрий <@DESKTOP-N4D3ITB>
2019-03-25 22:48:35 +0400

    Добавьте файлы проекта.

---
+++ MPI/Task_2.cpp
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "mpi.h"
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int * argc, char ***argv){
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int rank, resultlen;
+	int* buffer = (int*) malloc(sizeof(int));
+	char name[MPI_MAX_PROCESSOR_NAME];
+	int i = 0;
+
+	MPI_Init(argc, argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Get_processor_name(name, &resultlen);
+	 do {
+		 
+		if (rank == 0) {
+			printf("Enter number: ");
+			fflush(stdout);
+			scanf("%d", buffer);
+		}
+
+		MPI_Barrier(MPI_COMM_WORLD);
+		MPI_Bcast(buffer, 1, MPI_INT, 0, MPI_COMM_WORLD);
+		printf("Process: %d; buffer: %d\n", rank, *buffer);
+		fflush(stdout);
+		MPI_Barrier(MPI_COMM_WORLD);
+	 } while (*buffer > 0);
+
+	MPI_Finalize();
+	return 0;
+}
